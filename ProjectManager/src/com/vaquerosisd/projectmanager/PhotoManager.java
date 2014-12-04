6fea18643ddbfcf084c45c0092557753c3e497a4
JFernandoLuna <jfernando.luna91@gmail.com>
2014-12-03 23:08:15 -0600

    Video fragment content completed V1.0. UX enhacement on PhotoManager

--- ProjectManager/src/com/vaquerosisd/projectmanager/PhotoManager.java
+++ ProjectManager/src/com/vaquerosisd/projectmanager/PhotoManager.java
@@ -18,7 +18,6 @@ import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
